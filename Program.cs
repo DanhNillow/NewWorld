f9a912446cd61d83827b2567a8519a72aab73af5
Danillo Pontes Alves <danillopontesalves@gmail.com>
2022-01-20 20:57:36 -0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   // Apenas uma área para escrever //
+            /*
+             Int - Variável de números inteiros
+             Float - Variável de números decimais
+             Boll - Variável (Verdadeiro ou Falso)
+             String - Variável Texto
+             Char - Caractere
+            */
+
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Meu Nome é Danillo");
+
+            Console.ReadLine();
+
+        }
+    }
+}
